da361db092cdea05ff474441a04147494db59839
Benjamin <ben.d.maryland@gmail.com>
2021-09-21 19:41:00 -0400

    commit tree 7cdd3d1e4a8ae42e3e1b74553d92778d7825df37

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+var bestCustomer 
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  return bestCustomer;
+}
+
+function overwriteBestCustomer(x){
+ bestCustomer = x
+return bestCustomer
+}
+const leastFavoriteCustomer = "Bobby"
+
+function changeLeastFavoriteCustomer(y) {
+    leastFavoriteCustomer = y
+    return leastFavoriteCustomer
+}
